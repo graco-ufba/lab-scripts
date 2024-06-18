dfcf61c74e2fdb4fde50d65d8112412dbaec8012
Luís Bertoli <104996143+bertolima@users.noreply.github.com>
2024-06-18 14:55:23 -0300

    garantindo privilegios da vm a aluno

--- lab-startup.sh
+++ lab-startup.sh
@@ -274,11 +274,11 @@ fi
 #Adicionando VM
 sudo apt-get install mokutil
 SBDISABLED=$(mokutil --sb)
-if [ "$SBDISABLED" = "SecureBoot disabled" || [[ ! -d /opt/Linux ]]; then
-    sudo apt-get install virtualbox -y
-    if [ -f /home/suporte/Downloads/Linux.ova ]; then
+if [ "$SBDISABLED" = "SecureBoot disabled" ]; then
+    sudo apt-get install virtualbox
+    if [ -f /home/suporte/Downloads/Linux.ova -a ! -d /opt/Linux ]; then
         vboxmanage import /home/suporte/Downloads/Linux.ova --vsys 0 --basefolder "/opt"
-	chmod -R 777 /opt/Linux
+	chown -R aluno:aluno /opt/Linux
     fi
 fi
 
