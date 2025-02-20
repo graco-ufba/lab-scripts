4973da455b3a7b22e3e8322c6f9abe4a31cb5e53
Bruno Silva <134339999+BrunoSDMDev@users.noreply.github.com>
2025-02-20 10:23:30 -0300

    Add wget lab-admin-profile-config.sh

--- lab-startup.sh
+++ lab-startup.sh
@@ -8,6 +8,8 @@ wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/main/lab-programs.
 wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/main/lab-eula-programs.sh -O /tmp/lab-eula-programs.sh
 wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/main/lab-program-config.sh -O /tmp/lab-program-config.sh
 wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/main/lab-inventory.sh -O /tmp/lab-inventory.sh
+wget https://raw.githubusercontent.com/graco-ufba/lab-scripts/main/lab-admin-profile-config.sh -O /tmp/lab-admin-profile-config.sh
+
 
 
 if ! [ -f /usr/local/sbin/done.txt ]; then
