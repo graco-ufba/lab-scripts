2403effec3e49e64c120ddc01e63d1b325f2a030
BiancoCarvalho <biancocarvalho@hotmail.com>
2025-03-11 11:24:12 -0300

    Update lab-eula-programs.sh

--- lab-eula-programs.sh
+++ lab-eula-programs.sh
@@ -4,12 +4,35 @@ export DEBIAN_FRONTEND=noninteractive
 
 
 # VirtualBox
-sudo apt-get install mokutil
+#sudo apt-get install mokutil
+#SBDISABLED=$(mokutil --sb)
+#if [ "$SBDISABLED" = "SecureBoot disabled" ]; then
+#    sudo apt install virtualbox -y
+#    echo virtualbox-ext-pack virtualbox-ext-pack/license select "true" | sudo debconf-set-selections
+#    sudo apt install virtualbox-ext-pack -y
+#fi
+
+sudo apt-get install mokutil -y
 SBDISABLED=$(mokutil --sb)
+
 if [ "$SBDISABLED" = "SecureBoot disabled" ]; then
-    sudo apt install virtualbox -y
-    echo virtualbox-ext-pack virtualbox-ext-pack/license select "true" | sudo debconf-set-selections
-    sudo apt install virtualbox-ext-pack -y
+    echo "Removendo versões antigas do VirtualBox..."
+    sudo apt remove --purge virtualbox virtualbox-6.1 virtualbox-6.0 virtualbox-5.* -y
+    sudo apt autoremove -y
+
+    echo "Adicionando repositório oficial do VirtualBox 7.x..."
+    wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo tee /etc/apt/trusted.gpg.d/oracle_vbox_2016.asc
+    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo tee /etc/apt/trusted.gpg.d/oracle_vbox.asc
+    echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+
+    sudo apt update
+    sudo apt install -y virtualbox-7.0
+
+    echo "Instalando o Extension Pack do VirtualBox 7..."
+    VBOX_VERSION=$(VBoxManage -v | cut -dr -f1)
+    wget "https://download.virtualbox.org/virtualbox/$VBOX_VERSION/Oracle_VM_VirtualBox_Extension_Pack-$VBOX_VERSION.vbox-extpack" -O /tmp/extension_pack.vbox-extpack
+    sudo VBoxManage extpack install --replace /tmp/extension_pack.vbox-extpack
+    rm /tmp/extension_pack.vbox-extpack
 fi
 
 #wireshark
